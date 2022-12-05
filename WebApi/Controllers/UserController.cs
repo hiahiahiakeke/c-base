b858c56069bcce8921b48e4488c687ce2a841b9f
liuke <hiahiahiakeke@163.com>
2022-12-05 17:17:13 +0800

    change to 2022

---
+++ WebApi/Controllers/UserController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApi.Controllers
+{
+    [Route("[controller]/[Action]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        public string Get()
+        {
+            return "这是一个get请求";
+        }
+    }
+
+}
