e7689ed6d0614cb5ecbcf72270623513fb9729a3
liuke <hiahiahiakeke@163.com>
2022-12-05 13:40:28 +0800

    new project

---
+++ WebApi/WebApi/Controllers/UserController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApi.Controllers
+{
+    [Route("[controller]/[Action]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        [HttpGet]
+        public string Get()
+        {
+            return "这是一个get请求";
+        }
+    }
+}
